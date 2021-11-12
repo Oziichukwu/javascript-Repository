832974f8136a9f102db36caa90c4ef88355ebb49
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-12 20:08:08 +0100

    spread operator

---
+++ spread.js
@@ -0,0 +1,14 @@
+
+let persons = {
+
+    person1: "jerry",
+    person2: "saheed",
+    person3: "demola",
+    person4: "dami"
+}
+
+console.log("1", persons)
+
+persons = {...persons}
+
+console.log("2", persons)
